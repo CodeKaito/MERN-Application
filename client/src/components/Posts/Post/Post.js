8e0157954fc9b905343766ab21370558c2ea5ec3
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-13 10:40:49 +0100

    update

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,6 +8,7 @@ import { useDispatch } from 'react-redux';
 
 import { likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
+const classes = useStyles();
 
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
