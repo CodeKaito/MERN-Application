774cc22b1d8324217ef9da2c443f7b782752f96b
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-13 10:05:52 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import memories from './images/memories.png';
 
 function App() {
   return (
     <Container maxidth='lg'>
       <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'> Memo </Typography>
+        <Typography variant='h2' align='center'> Memories </Typography>
+        <img src={memories} alt='memories' height='60'/>
       </AppBar>
     </Container>
   );
